52dc954656d54275cd6ec38313d67d766a79ddd8
chaitanya potti <chaitanya.potti@gmail.com>
2018-08-17 17:00:56 +0530

    change

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,8 @@
 import Web3 from "web3";
 
 let web3;
-if (typeof web3 !== "undefined") web3 = new Web3(window.web3.currentProvider);
+if (typeof window.web3 !== "undefined")
+  web3 = new Web3(window.web3.currentProvider);
 else {
   console.log("Install Metamask");
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
