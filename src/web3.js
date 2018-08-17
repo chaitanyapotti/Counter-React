3e5c684348807d7edc980ec6202c580b48fe9aba
chaitanya potti <chaitanya.potti@gmail.com>
2018-08-17 16:59:58 +0530

    handle web3

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,9 @@
 import Web3 from "web3";
 
 let web3;
-if (window.web3 != null) web3 = new Web3(window.web3.currentProvider);
-else console.log("Install Metamask");
+if (typeof web3 !== "undefined") web3 = new Web3(window.web3.currentProvider);
+else {
+  console.log("Install Metamask");
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
 export default web3;
