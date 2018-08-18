5b8e7f876c0d7a699513e83109383b00d1221857
chaitanya potti <chaitanya.potti@gmail.com>
2018-08-18 21:25:01 +0530

    added token balances

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,25 +3,23 @@ import { Card } from "reactstrap";
 import web3 from "../helpers/web3";
 import Form from "./Form";
 import { Grid, Col, Row } from "../helpers/Grid";
-import { Header } from "../components";
 import counterErcKovan from "../helpers/contractInstances/counterErcKovan";
 import counterErcRinkeby from "../helpers/contractInstances/counterErcRinkeby";
-import {Header, ModalComponent, Trade, Claim} from '../components';
+import { Header, ModalComponent, Trade, Claim } from "../components";
 
 class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+      network: "",
+      kovanBalance: 0,
+      rinkebyBalance: 0,
+      modal: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            account: "",
-            network: "",
-            kovanBalance: 0,
-            rinkebyBalance: 0,
-            modal: false
-        };
-        this.toggle = this.toggle.bind(this);
-    }
-  
   async componentDidMount() {
     const account = await web3.eth.getAccounts();
     const network = await web3.eth.net.getNetworkType();
@@ -51,49 +49,66 @@ class Landing extends Component {
 
   render() {
     return (
-        <div className="landing-img">
-            <Header account={this.state.account}/>
-            <Grid>
-                <Row>
-                <Col lg={8} xs={12} style={{height:'100vh'}}></Col>
-                <Col lg={4} xs={12} style={{height:'100vh'}} className="push-top--150">
-                    <Card style={{padding: '40px'}}>
-                        <div className="fnt-65 txt-grad">COUNTER</div>
-                        <div className="txt-xxxxl opacity-50 mrgn-landng-txt">OTC Trades made easy</div>
-                        {this.state.account !== undefined ? (
-                        <div>
-                            {
-                            this.state.network === "kovan"?
-                                <div className="push-top--45 txt-xxl">
-                                    <div>Network Type - Kovan</div>
-                                    <div>Spv - 100</div>
-                                    <Trade class="push--top txt-l" onClick={this.toggle}/>
-                                    <Claim class="push--top push--left txt-l"  onClick={this.toggle}/>
-                                </div>
-                            :
-                                this.state.network === "rinkeby" &&
-                                    <div className="push-top--45 txt-xxl">
-                                        <div>RinkeBy</div>
-                                        <div>Ny to: 100</div>
-                                        <Trade class="push--top txt-l" onClick={this.toggle} />
-                                        <Claim class="push--top push--left txt-l" onClick={this.toggle}/>
-                                    </div>
-                                
-                            }
+      <div className="landing-img">
+        <Header account={this.state.account} />
+        <Grid>
+          <Row>
+            <Col lg={8} xs={12} style={{ height: "100vh" }} />
+            <Col
+              lg={4}
+              xs={12}
+              style={{ height: "100vh" }}
+              className="push-top--150"
+            >
+              <Card style={{ padding: "40px" }}>
+                <div className="fnt-65 txt-grad">COUNTER</div>
+                <div className="txt-xxxxl opacity-50 mrgn-landng-txt">
+                  OTC Trades made easy
+                </div>
+                {this.state.account !== undefined ? (
+                  <div>
+                    {this.state.network === "kovan" ? (
+                      <div className="push-top--45 txt-xxl">
+                        <div>Network Type - Kovan</div>
+                        <div>Token Name - Stokens Fund SPV</div>
+                        <div>Token Balance - {this.state.kovanBalance}</div>
+                        <Trade class="push--top txt-l" onClick={this.toggle} />
+                        <Claim
+                          class="push--top push--left txt-l"
+                          onClick={this.toggle}
+                        />
+                      </div>
+                    ) : (
+                      this.state.network === "rinkeby" && (
+                        <div className="push-top--45 txt-xxl">
+                          <div>Network Type - Rinkeby</div>
+                          <div>Token Name - New York Tower One</div>
+                          <div>Token Balance - {this.state.rinkebyBalance}</div>
+                          <Trade
+                            class="push--top txt-l"
+                            onClick={this.toggle}
+                          />
+                          <Claim
+                            class="push--top push--left txt-l"
+                            onClick={this.toggle}
+                          />
                         </div>
-                        ) : (
-                        <div>
-                            Please unlock/install metamask and then refresh the page
-                        </div>
-                        )}
-                    </Card>
-                </Col>
-                </Row>
-            </Grid>
-            <ModalComponent toggle={this.toggle} modal={this.state.modal}>
-                <Form />
-            </ModalComponent>
-        </div>
+                      )
+                    )}
+                  </div>
+                ) : (
+                  <div>
+                    Please unlock/install metamask and then refresh the page
+                  </div>
+                )}
+              </Card>
+            </Col>
+          </Row>
+        </Grid>
+        <ModalComponent toggle={this.toggle} modal={this.state.modal}>
+          <Form />
+        </ModalComponent>
+      </div>
     );
   }
 }
